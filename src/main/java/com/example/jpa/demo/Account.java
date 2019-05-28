e2be2a62f4485adcbfd32bc4163327b124450ae1
ryan <wafy75@gmail.com>
2019-05-28 22:48:21 +0900

    f

---
+++ src/main/java/com/example/jpa/demo/Account.java
@@ -0,0 +1,14 @@
+package com.example.jpa.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+}
